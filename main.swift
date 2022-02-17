9bb49a13eae57141008d709fdd401883fb2874ab
DARKSIDE <99494316+darkside-one@users.noreply.github.com>
2022-02-18 01:14:30 +0700

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,21 +1,18 @@
 func generateArrays(size: UInt8) -> [[Int]] {
 
-  // Generates an array of unique random numeric sizes
+  let intRange = Int(size)
   var setSizes = Set<UInt8>()
+  var resultArray = [[Int]]()
 
+  // Generates an array of unique random numeric sizes
   while setSizes.count < size {
     setSizes.insert(UInt8.random(in: 1...size + 10))
   }
   let uniqArraySize = Array(setSizes)
 
   // Filling an two-dimensional array of sorted arrays
-  var randomValuesArray = [Int]()
-  var resultArray = [[Int]]()
-
-  let intRange = Int(size)
-
   for (index, size) in uniqArraySize.enumerated() {
-
+    var randomValuesArray = [Int]()
     for _ in 0..<size {
       randomValuesArray.append(Int.random(in: -10 - intRange...intRange + 10))
     }
@@ -23,8 +20,6 @@ func generateArrays(size: UInt8) -> [[Int]] {
     index.isMultiple(of: 2)
       ? resultArray.append(randomValuesArray.sorted(by: <))
       : resultArray.append(randomValuesArray.sorted(by: >))
-
-    randomValuesArray = [Int]()
   }
   return resultArray
 }
