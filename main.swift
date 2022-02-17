0ee737e3b2681f004467640d41c5f900c122a382
DARKSIDE <99494316+darkside-one@users.noreply.github.com>
2022-02-17 23:54:08 +0700

    Update main.swift

--- main.swift
+++ main.swift
@@ -1,55 +1,31 @@
-import Foundation
-
-// MARK: - Methods
 func createAndSortArrays (size: Int) -> [[Int]] {
     
-    guard size > 0 else { return [[Int]]() }
+    guard size > 0 else { return [] }
     
     // Generates an array of unique random numeric sizes
     func uniqArraySize(size: Int) -> [Int] {
-        var array = [Int]()
-
-        while array.count < size {
-        for _ in 0..<size {
-            let randomValue = Int.random(in: 1...size)
-            
-            if !array.contains(randomValue) {
-                array.append(randomValue)
-            }
+        var setSizes = Set<Int>()
+        
+        while setSizes.count < size {
+            setSizes.insert(Int.random(in: 1...size))
         }
-        }
-        return array
+        return Array(setSizes)
     }
     
     // Generates an arrays of random numbers
     func getRandValue(size: Int) -> [Int] {
-        var result = [Int]()
-        
-        for _ in 0..<size {
-            let randomValue = Int.random(in: -10-size...size+10)
-            result.append(randomValue)
-        }
-        return result
+        return (0..<size).map { _ in Int.random(in: -10-size...size+10) }
     }
-    
+
     // Filling an two-dimensional array of sorted arrays
     let uniqueNumArray = uniqArraySize(size: size)
 
-    print(uniqueNumArray)
-
     var resultArray = [[Int]]()
-    var index = 0
-    
-    for i in uniqueNumArray {
-      index % 2 != 0 ? resultArray.append(getRandValue(size: i).sorted().reversed())
-        : resultArray.append(getRandValue(size: i).sorted())
-        
-        index += 1
+
+    for (index, size) in uniqueNumArray.enumerated() {
+      index.isMultiple(of: 2) 
+        ? resultArray.append(getRandValue(size: size).sorted(by: <))
+        : resultArray.append(getRandValue(size: size).sorted(by: >))
     }
     return resultArray
 }
-
-// MARK: - Properties
-var test = createAndSortArrays(size: 5)
-
-print(test)
